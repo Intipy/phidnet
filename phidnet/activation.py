76bcaa9da4b5bd471ed5f45463c109034c287c65
Intipy <jios6790@gmail.com>
2021-08-12 12:35:59 +0900

    Update activation.py

--- phidnet/activation.py
+++ phidnet/activation.py
@@ -4,15 +4,11 @@ import numpy as np
 
 class Sigmoid:  # Sigmoid class
     def __init__(self):
-        self.mask = None
-        self.mask1 = None
         self.out = None
 
     def forward(self, x):    # Sigmoid function
-        self.mask = (x > 100)
-        self.mask1 = (x < -100)
-        x[self.mask] = 100
-        x[self.mask1] = -100
+        x[x > 1000] = 1000
+        x[x < -1000] = -1000
         self.out = 1 / (1 + np.exp(-x))
         return self.out
 
@@ -24,43 +20,33 @@ class Sigmoid:  # Sigmoid class
 
 class Relu:   # ReLU class
     def __init__(self):
-        self.mask = None
-        self.mask1 = None
         self.out = None
 
     def forward(self, x):   # ReLU function
-        self.mask = (x <= 0)
         self.out = x.copy()
-        self.out[self.mask] = 0
+        self.out[x < 0] = 0
         return self.out
 
     def backward(self, x):   # derivative of ReLU function
-        self.mask = (x <= 0)
-        self.mask1 = (x > 0)
         self.out = x.copy()
-        self.out[self.mask] = 0
-        self.out[self.mask1] = 1
+        self.out[x < 0] = 0
+        self.out[x > 0] = 1
         return self.out
 
 
 
 class Softmax:   # Softmax class
     def __init__(self):
-        self.mask = None
-        self.mask1 = None
         self.out = None
 
     def forward(self, x):   # Softmax function
-        self.mask = (x > 100)
-        self.mask1 = (x < -100)
-        x[self.mask] = 100
-        x[self.mask1] = -100
+        x[x > 1000] = 1000
+        x[x < -1000] = -1000
         self.out = np.exp(x) / np.sum(np.exp(x))
         return self.out
 
     def backward(self, x):   # derivative of softmax function
-        self.out = self.forward(x) * (1 - self.forward(x))
-        return self.out
+        pass
 
 
 
@@ -69,7 +55,7 @@ class Linear:   # Linear class
         self.out = None
 
     def forward(self, x):   # Linear function
-        self.out = x
+        self.out = x.copy()
         return self.out
 
     def backward(self, x):   # derivative of linear function
