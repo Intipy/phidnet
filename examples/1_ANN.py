000f61e1009c0395880d7c7be8875452540eb982
Intipy <jio6790@gmail.com>
2021-09-04 16:58:27 +0900

    git commit

--- examples/1_ANN.py
+++ examples/1_ANN.py
@@ -1,4 +1,4 @@
-import numpy as np; np.random.seed(121);
+import numpy as np; np.random.seed(121)
 from matplotlib import pyplot as plt
 import phidnet   # Import phidnet
 
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding='UTF-8') as fh:
 setuptools.setup(
     
     name="phidnet",
-    version="0.0.6",
+    version="0.0.7",
     author="Intipy",
     author_email="jios6790@gmail.com",
     description="Phidnet",
